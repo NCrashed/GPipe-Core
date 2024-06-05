2cfff66505770d92aa3e347e56a72ad9f8d5dba6
Anton Gushcha <ncrashed@protonmail.com>
2024-06-05 17:31:50 +0700

    Add fields for pairs and tripples of coords

--- GPipe-Core/src/Data/Linear/Fields.hs
+++ GPipe-Core/src/Data/Linear/Fields.hs
@@ -15,6 +15,7 @@
 -- @
 -----------------------------------------------------------------------------
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE InstanceSigs #-}
 module Data.Linear.Fields where 
 
 import GHC.Records 
@@ -49,3 +50,57 @@ instance HasField "z" (V4 a) a where
 
 instance HasField "w" (V4 a) a where 
     getField (V4 _ _ _ w) = w
+
+instance HasField "xy" (V2 a) (V2 a) where 
+    getField = id 
+
+instance HasField "xy" (V3 a) (V2 a) where 
+    getField (V3 x y _) = V2 x y 
+
+instance HasField "xy" (V4 a) (V2 a) where 
+    getField (V4 x y _ _) = V2 x y 
+
+instance HasField "yx" (V2 a) (V2 a) where 
+    getField (V2 x y) = V2 y x
+
+instance HasField "yx" (V3 a) (V2 a) where 
+    getField (V3 x y _) = V2 y x 
+
+instance HasField "yx" (V4 a) (V2 a) where 
+    getField (V4 x y _ _) = V2 y x 
+
+instance HasField "xyz" (V3 a) (V3 a) where 
+    getField = id
+
+instance HasField "xyz" (V4 a) (V3 a) where 
+    getField (V4 x y z _) = V3 x y z
+
+instance HasField "yxz" (V3 a) (V3 a) where 
+    getField (V3 x y z)= V3 y x z
+
+instance HasField "yxz" (V4 a) (V3 a) where 
+    getField (V4 x y z _) = V3 y x z
+
+instance HasField "zxy" (V3 a) (V3 a) where 
+    getField (V3 x y z)= V3 z x y
+
+instance HasField "zxy" (V4 a) (V3 a) where 
+    getField (V4 x y z _) = V3 z x y
+
+instance HasField "zyx" (V3 a) (V3 a) where 
+    getField (V3 x y z)= V3 z y x
+
+instance HasField "zyx" (V4 a) (V3 a) where 
+    getField (V4 x y z _) = V3 z y x
+
+instance HasField "xzy" (V3 a) (V3 a) where 
+    getField (V3 x y z)= V3 x z y
+
+instance HasField "xzy" (V4 a) (V3 a) where 
+    getField (V4 x y z _) = V3 x z y
+
+instance HasField "yzx" (V3 a) (V3 a) where 
+    getField (V3 x y z)= V3 y z x
+
+instance HasField "yzx" (V4 a) (V3 a) where 
+    getField (V4 x y z _) = V3 y z x
